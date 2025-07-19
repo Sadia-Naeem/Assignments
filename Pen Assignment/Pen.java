698f00551005101264da13035dbfbc53a19cd253
Sadia Naeem <sadia.naeem.kk@gmail.com>
2025-07-19 19:46:14 +0500

    Add files via upload

---
+++ Pen Assignment/Pen.java
@@ -0,0 +1,14 @@
+public class Pen{
+      String color;
+      String type;   // type such as ink pen, gel pen
+
+public void write() {
+    System.out.println("This is the info for Dollar and Piano");
+}
+public void printColor() {
+    System.out.println(this.color);
+}
+public void printType() {
+    System.out.println(this.type);
+     }
+ }
---
+++ Pen Assignment/main.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String[] args) {
+        Pen Dollar = new Pen();
+        Dollar.color = "blue"; 
+        Dollar.type = "ink pen";
+       
+        Dollar.write();
+        Dollar.printColor();
+        Dollar.printType();
+
+        Pen Piano = new Pen();
+        Piano.color = "black";
+        Piano.type = "gel pen";
+        
+        Piano.printColor();
+        Piano.printType();
+ 
+    }
+}
